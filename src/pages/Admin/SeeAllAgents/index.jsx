00519193515098c747fa3aa426b197a36f6a7300
sirconfy <csirconfy@gmail.com>
2023-08-22 13:46:40 -0700

    agent table list

--- src/pages/Admin/SeeAllAgents/index.jsx
+++ src/pages/Admin/SeeAllAgents/index.jsx
@@ -1,17 +1,24 @@
 import AgentList from "@/Components/AgentList";
 import Axios from "@/utils/Axios";
-
+import { Button } from "@/components/ui/button"
+import moment from 'moment/moment';
+import { DataTable } from '@/SharedComponents/DataTable'
+import { Pagination } from '@/SharedComponents/Pagintion'
+import { SearchBar } from "@/SharedComponents/SearchBar"
 import Cookies from "js-cookie";
 import Image from "next/image";
+import '../../../styles/local.css'
 import React, { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import { BiLoader } from "react-icons/bi";
+import { FaTrash } from 'react-icons/fa';
 
 const agenturl = "/agents";
 
 const Index = () => {
   const [Loading, setLoading] = useState(false);
   const [agents, setAgents] = useState([]);
+
 
   useEffect(() => {
     const fetchAgents = async () => {
@@ -44,11 +51,77 @@ const Index = () => {
   const HandleDeleteItem = (AgentID) => {
     setAgents((previtem) => {
       const filteredArray = previtem.filter((item) => item._id !== AgentID);
-      toast.success("deleted");
+      toast.success("Deleted");
       return filteredArray;
     });
   };
+  
 
+
+
+   const columns = [
+        {
+          accessorKey: "serialNumber",
+          header: "S/N",
+        },
+        {
+          accessorKey: "firstname",
+          header: "First Name",
+        },
+        {
+          accessorKey: "lastname",
+          header: "Last Name",
+        },
+        {
+          accessorKey: "email",
+          header: "Email",
+        },
+
+        {
+          accessorKey: "company",
+          header: "Company",
+        },
+        {
+          accessorKey: "phone",
+          header: "Phone",
+        },
+        {
+          accessorKey: "createdAt",
+          header: "Date Created",
+          valueFormatter: (params) => moment(params.value).format('YYYY-MM-DD'),
+        },
+        {
+          accessorKey: "account_status",
+          header: "Status",
+          cellRenderer: ({ value }) => (
+            <div
+              className={`status-cell ${
+                value === "approved" ? "bg-green-500" : "bg-orange-500"
+              } text-white p-2 rounded`}
+            >
+              {value}
+            </div>
+          ),
+        },
+        {
+          accessorKey: "actions",
+          header: "Actions",
+          cellRenderer: ({ row }) => (
+            <div className="flex items-center space-x-2">
+              <FaTrash
+                className="cursor-pointer text-red-600 hover:text-red-800"
+                onClick={() => HandleDeleteItem(row._id)}
+              />
+            </div>
+          ),
+        },
+      ] 
+
+   // Add a serial number field to each entry
+const dataWithSerialNumbers = agents.map((entry, index) => ({
+  ...entry,
+  serialNumber: index + 1,
+}));
   return (
     <>
       {Loading ? (
@@ -63,7 +136,7 @@ const Index = () => {
       ) : (
         <section className="bg-[#ebeefd] h-full fixed w-full overflow-auto">
           <div className="container body-content">
-            <p className="admin-header-text">See all Agents</p>
+            <p className="admin-header-text ">See all Agents</p>
 
             {agents.length === 0 ? (
               <div className="fixed h-screen w-screen top-0 left-0 flex items-center justify-center text-4xl font-bold capitalize">
@@ -71,13 +144,19 @@ const Index = () => {
               </div>
             ) : (
               <div className="see-all-div">
-                <AgentList
+                {/* <AgentList
                   Agentdata={agents}
                   onDelete={(id) => {
                     HandleDeleteItem(id);
                   }}
                   key={agents.length}
-                />
+                /> */}
+                <div className="container mx-auto py-10">
+    <SearchBar />
+   <DataTable columns={columns} data={dataWithSerialNumbers}/>
+   <Pagination/>
+
+   </div>
               </div>
             )}
           </div>
